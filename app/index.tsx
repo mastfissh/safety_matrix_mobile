57da86c24b5fc574a8e4895c641a63705775e8d7
mastfish <mastfish@gmail.com>
2024-08-28 00:36:50 -0400

    wip on adding data

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,22 @@
-import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { withExpoSnack } from 'nativewind';
+import data from "../assets/data/data.json";
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
 
-export default function App() {
+const StyledView = styled(View)
+const StyledText = styled(Text)
+
+const App = () => {
   return (
-    <WebView
-      style={styles.container}
-      source={{ uri: 'https://psychcombo.com/' }}
-    />
+    <StyledView className="flex-1 items-center justify-center">
+      <StyledText className="text-slate-800">Try editing me!! 🎉</StyledText>
+      <StyledText className="text-slate-800">{data["drugs"][0]}</StyledText>
+    </StyledView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
-});
+// This demo is using a external compiler that will only work in Expo Snacks.
+// You may see flashes of unstyled content, this will not occur under normal use!
+// Please see the documentation to setup your application
+export default withExpoSnack(App);
