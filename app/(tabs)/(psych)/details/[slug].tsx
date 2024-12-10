fc3c4754456fb63359d2beb35d6144d56f119f86
Justin <mastfish@gmail.com>
2024-12-10 17:50:01 +1100

    add chart

--- app/(tabs)/(psych)/details/[slug].tsx
+++ app/(tabs)/(psych)/details/[slug].tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import psychoactives from "../../../../assets/data/psychoactives.json";
 import Markdown from "react-native-marked";
 import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { Image } from 'expo-image';
+import {LineGraph} from 'react-native-graph';
 
 let idx = {} as any;
 for (let sub of psychoactives) {
@@ -12,11 +13,27 @@ for (let sub of psychoactives) {
 
 const App = () => {
   const { slug } : {slug:string} = useLocalSearchParams();
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  const next = new Date();
+  next.setDate(today.getDate() + 2);
+  const series = [{
+    value: 1,
+    date: today
+  },{
+    value: 4,
+    date: tomorrow
+  },{
+    value: 3,
+    date: next
+  }]
   const record = idx[slug]
   const entry = record
   return (
+    <ScrollView>
     <View className="flex-1 items-center justify-center">
-       <Image source={"i_"+ (slug as string).replaceAll('-', "_")}  style={{ width: 50, height: 50 }}/>
+       <Image source={"i_"+ (slug as string).replaceAll('-', "_")}  style={{ width: 500, height: 50 }}/>
       <Text className="text-slate-800">Details for {record.data.title}</Text>
       <Text> {entry.data.title} </Text>
    
@@ -32,17 +49,28 @@ const App = () => {
     {entry.data.family_members &&
     <Text>{entry.data.title} is not just one drug; {entry.data.title} is a group of drugs. Drugs in this group include {entry.data.family_members.join(', ')}. Drugs within the same group can have very different dosages and very different effects.</Text>
   }
+  
      <Text> Effects </Text>
      <Text>{entry.data.positive_effects}</Text>
      <Text> {entry.data.neutral_effects}</Text>
      <Text> {entry.data.negative_effects}</Text>
+     
       <Markdown
       value={record.body}
       flatListProps={{
         initialNumToRender: 8,
       }}
     />
+    <LineGraph
+    key="graph"
+  points={series}
+  animated={false}
+  color="#4484B2"
+  className="absolute align-middle mb-4"
+  style={{ width: 50, height: 50 }}
+/>
     </View>
+    </ScrollView>
   );
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from 'expo-router/stack';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // Import your global CSS file
 import "../global.css";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
