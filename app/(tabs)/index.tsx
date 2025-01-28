11d33e6d729d09df1bf3cd8603c05a315cb3ccba
Justin <mastfish@gmail.com>
2025-01-28 16:09:27 +1100

    sectionlist ify

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,10 +9,11 @@ import {
   ScrollView,
   Modal,
   Pressable,
+  SectionList,
 } from "react-native";
 import { risk, risk_to_bg } from "../../lib/util";
 import { Link } from "expo-router";
-import AntDesign from '@expo/vector-icons/AntDesign';
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 const list: any = {};
 const mainlist: any[] = [];
@@ -125,88 +126,122 @@ const App = () => {
       >
         <Text className="text-white font-bold text-center">Show Modal</Text>
       </Pressable>
-      <FlatList
-        key={JSON.stringify(chart)}
-        data={grid}
-        numColumns={chart.length}
-        renderItem={Item}
+      <SectionList
+        sections={[
+          {
+            title: "Chart",
+            data: [grid],
+            renderItem: ({ item }) => (
+              <FlatList
+                key={JSON.stringify(chart)}
+                data={item}
+                numColumns={chart.length}
+                renderItem={Item}
+              />
+            ),
+          },
+          {
+            title: "Information",
+            data: ["" as any],
+            renderItem: ({}) => (
+              <Fragment>
+                <View className="container px-6 mx-auto">
+                  <View className="mb-2 text-gray-800">
+                    <View className="grow-0 shrink-0 basis-auto">
+                      <View className="max-w-2xl mx-auto">
+                        <Text className="text-xl font-bold mb-4">
+                          Psychoactive Combination Matrix
+                        </Text>
+                        <Text className="text-md font-bold mb-4">
+                          What this chart tells you
+                        </Text>
+                        <Text className="text-gray-500 mb-6">
+                          How risky it is when you combine two psychoactives.
+                        </Text>
+                        <Text className="text-md font-bold mb-4">
+                          How this chart works
+                        </Text>
+                        <Text className="text-gray-500 mb-6">
+                          The coloured square where two psychoactives intersect
+                          on the grid is coded to show their combination risk.
+                        </Text>
+                        <Text className="text-gray-500 mb-6">
+                          Select psychoactives below to show them on the grid,
+                          or select psychoactives or combinations in the chart
+                          to learn more.
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+                <View className="block rounded-lg my-6 bg-gray-50 p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] max-w-2xl mx-auto">
+                  <Text className="text-xl font-bold">Key</Text>
+                  <Text>Risk = danger or hazard</Text>
+                  <Text>Synergy = increased effects, or new effects</Text>
+                  <Text>Decrease = reduced effects</Text>
+                </View>
+                <Text className="flex space-x-4">
+                  <AntDesign
+                    name="exclamationcircleo"
+                    size={24}
+                    color="black"
+                    className="h-7 w-7 inline"
+                  />
+                  = low confidence in the risk rating
+                </Text>
+                <View className="grid grid-cols-2 lg:grid-cols-3 ">
+                  <Text
+                    className={`${risk_to_bg(
+                      "SR"
+                    )} text-m font-medium m-2 px-2.5 py-0.5 rounded`}
+                  >
+                    Significant Risk
+                  </Text>
+                  <Text
+                    className={`${risk_to_bg(
+                      "GR"
+                    )} text-m font-medium m-2 px-2.5 py-0.5 rounded`}
+                  >
+                    Greater Risk
+                  </Text>
+                  <Text
+                    className={`${risk_to_bg(
+                      "MR"
+                    )} text-m font-medium m-2 px-2.5 py-0.5 rounded`}
+                  >
+                    Minor Risk
+                  </Text>
+                  <Text
+                    className={`${risk_to_bg(
+                      "LRS"
+                    )} text-m font-medium m-2 px-2.5 py-0.5 rounded`}
+                  >
+                    Low Risk Synergy
+                  </Text>
+                  <Text
+                    className={`${risk_to_bg(
+                      "LRD"
+                    )} text-m font-medium m-2 px-2.5 py-0.5 rounded`}
+                  >
+                    Low Risk Decrease
+                  </Text>
+                  <Text
+                    className={`${risk_to_bg(
+                      "LRNS"
+                    )} text-m font-medium m-2 px-2.5 py-0.5 rounded`}
+                  >
+                    Low Risk No Synergy
+                  </Text>
+                </View>
+              </Fragment>
+            ),
+          },
+        ]}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-lg font-bold mb-2">{title}</Text>
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
-      <View className="container px-6 mx-auto">
-        <View className="mb-2 text-gray-800">
-          <View className="grow-0 shrink-0 basis-auto">
-            <View className="max-w-2xl mx-auto">
-              <Text className="text-xl font-bold mb-4">
-                Psychoactive Combination Matrix
-              </Text>
-              <Text className="text-md font-bold mb-4">
-                What this chart tells you
-              </Text>
-              <Text className="text-gray-500 mb-6">
-                How risky it is when you combine two psychoactives.
-              </Text>
-              <Text className="text-md font-bold mb-4">
-                How this chart works
-              </Text>
-              <Text className="text-gray-500 mb-6">
-                The coloured square where two psychoactives intersect on the
-                grid is coded to show their combination risk.
-              </Text>
-              <Text className="text-gray-500 mb-6">
-                Select psychoactives below to show them on the grid, or select
-                psychoactives or combinations in the chart to learn more.
-              </Text>
-            </View>
-          </View>
-        </View>
-      </View>
-      <View className="block rounded-lg my-6 bg-gray-50 p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] max-w-2xl mx-auto">
-        <Text className="text-xl font-bold">Key</Text>
-        <Text>Risk = danger or hazard</Text>
-        <Text>Synergy = increased effects, or new effects</Text>
-        <Text>Decrease = reduced effects</Text>
-      </View>
-      <Text className="flex space-x-4">
-      <AntDesign name="exclamationcircleo" size={24} color="black" className="h-7 w-7 inline" />
-        = low confidence in the risk rating
-      </Text>
-      <View className="grid grid-cols-2 lg:grid-cols-3">
-        <Text
-          className={risk_to_bg("SR")}
-          // className="text-m font-medium m-2 px-2.5 py-0.5 rounded"
-        >
-          Significant Risk
-        </Text>
-        <Text
-          className={risk_to_bg("GR")}
-          // className="text-m font-medium m-2 px-2.5 py-0.5 rounded"
-        >
-          Greater Risk
-        </Text>
-        <Text
-          className={risk_to_bg("MR")}
-          // className="text-m font-medium m-2 px-2.5 py-0.5 rounded"
-        >
-          Minor Risk
-        </Text>
-        <Text
-          className={risk_to_bg("LRS")}
-          // className="text-m font-medium m-2 px-2.5 py-0.5 rounded"
-        >
-          Low Risk Synergy
-        </Text>
-        <Text
-          className={risk_to_bg("LRD")}
-          // className="text-m font-medium m-2 px-2.5 py-0.5 rounded"
-        >
-          Low Risk Decrease
-        </Text>
-        <Text
-          className={risk_to_bg("LRNS")}
-          // className="text-m font-medium m-2 px-2.5 py-0.5 rounded"
-        >
-          Low Risk No Synergy
-        </Text>
-      </View>
     </View>
   );
 };
