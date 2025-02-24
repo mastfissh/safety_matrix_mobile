65b4689f97a16145ec3abd980a179c93dae45079
Justin <mastfish@gmail.com>
2025-02-24 15:37:45 +1100

    allow landscape

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,40 @@
 import { Stack } from "expo-router/stack";
-import { Platform } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
+import React, { useEffect } from "react";
 
 import "@/global.css";
 
+async function unlockOrientationForTablets() {
+  const deviceType = await Device.getDeviceTypeAsync();
+  if (deviceType === Device.DeviceType.TABLET) {
+    await ScreenOrientation.unlockAsync();
+  }
+}
+
+
 export default function Layout() {
   const headerShown = Platform.OS === "android" ? false : true;
-
+  useEffect(() => {
+    unlockOrientationForTablets();
+  }, []);
+  // useEffect(() => {
+  //   const subscription = ScreenOrientation.addOrientationChangeListener(
+  //     // handleOrientationChange
+  //   );
+  //   return () => {
+  //     ScreenOrientation.removeOrientationChangeListeners();
+  //   };
+  // }, []);
+  
+  // const handleOrientationChange = (orientationChange) => {
+  //   const currentOrientation = orientationChange.orientationInfo.orientation;
+  //   // Adjust your layout based on the currentOrientation
+  // };
+  
+  
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack
