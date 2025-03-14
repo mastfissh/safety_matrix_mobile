e48e1d75416c3689eb964088b773dfff91672a9a
Justin <mastfish@gmail.com>
2025-03-14 17:01:55 +1100

    mocks

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,34 @@
-export * from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+// __mocks__/async-storage-mock.js
+const AsyncStorageMock = {
+  getItem: jest.fn((key) => {
+    return new Promise((resolve, reject) => {
+      resolve(AsyncStorageMock.store[key] || null);
+    });
+  }),
+  setItem: jest.fn((key, value) => {
+    return new Promise((resolve, reject) => {
+      AsyncStorageMock.store[key] = value;
+      resolve();
+    });
+  }),
+  removeItem: jest.fn((key) => {
+    return new Promise((resolve, reject) => {
+      delete AsyncStorageMock.store[key];
+      resolve();
+    });
+  }),
+  clear: jest.fn(() => {
+    return new Promise((resolve, reject) => {
+      AsyncStorageMock.store = {};
+      resolve();
+    });
+  }),
+  getAllKeys: jest.fn(() => {
+    return new Promise((resolve, reject) => {
+      resolve(Object.keys(AsyncStorageMock.store));
+    });
+  }),
+  store: {},
+};
+
+export default AsyncStorageMock;
